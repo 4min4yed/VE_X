71371f231eddbf820c83f78a52ab836bca9f69c9
Mohamed Amin AYED <mohamedamin.ayed@supcom.tn>
2025-11-17 21:04:08 +0100

    FileUploads is Functional

---
+++ Backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile
+import hashlib
+import shutil
+import uuid
+import os
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+
+@app.post("/api/analyze")
+async def upload(file: UploadFile):
+    file_id = str(uuid.uuid4())
+    file_path = f"{UPLOAD_DIR}/{file_id}.exe"
+
+    # Save
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+    # Hash
+    sha256 = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for block in iter(lambda: f.read(4096), b""):
+            sha256.update(block)
+
+    digest = sha256.hexdigest()
+
+    return {"id": file_id, "hash": digest}
