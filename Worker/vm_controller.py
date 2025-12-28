1d9333202a8daec30a3cef6d30fcb67d4a347617
Mohamed Amin AYED <mohamedamin.ayed@supcom.tn>
2025-12-28 23:21:06 +0100

    Worker

---
+++ Worker/vm_controller.py
@@ -0,0 +1,8 @@
+import vbm_analysis_toolkit as vmt
+import vboxmanage_wrapper as vbm
+import vboxmanager as vbox
+
+
+
+def run_vm_analysis(filepath):
+    return f"Running VM analysis on {filepath}"
